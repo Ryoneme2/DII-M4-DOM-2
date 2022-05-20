366e354f8b8a302150441751806eb30087e04326
Ryoneme2 <worknarair6@gmail.com>
2022-05-20 11:17:28 +0700

    add username and gender

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let student = {}
-student.name = 'คุณลุง'
-student.username = 'a@b.com'
-student.gender = 'ชาย'
+let student = {};
+student.name = "คุณลุง";
+student.username = "a@b.com";
+student.gender = "ชาย";
 
-const addStudent = (student) => {
-  const output = document.getElementById('output')
-  let row = document.createElement('div')
-  row.classList.add('row')
-  let col = document.createElement('div')
-  col.classList.add('col-1')
-  col.classList.add('offset-1')
-  col.innerHTML = 'ชื่อ'
-  let colVal = document.createElement('div')
-  colVal.classList.add('col')
-  colVal.innerHTML = student.name
-  row.appendChild(col)
-  row.appendChild(colVal)
-  output.appendChild(row)
-}
+const addStudent = (student, prefix) => {
+  const output = document.getElementById("output");
+  let row = document.createElement("div");
+  row.classList.add("row");
+  let col = document.createElement("div");
+  col.classList.add("col-1");
+  col.classList.add("offset-1");
+  col.innerHTML = prefix;
+  let colVal = document.createElement("div");
+  colVal.classList.add("col");
+  colVal.innerHTML = student;
+  row.appendChild(col);
+  row.appendChild(colVal);
+  output.appendChild(row);
+};
 
-window.addEventListener('load', () => {
-  addStudent(student)
-})
+window.addEventListener("load", () => {
+  addStudent(student.name, "ชื่อ");
+  addStudent(student.username, "อีเมล");
+  addStudent(student.gender, "เพศ");
+});
